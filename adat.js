23797c0d620a88f7d4cff1679db7987b0f49488e
ewemormartin <martinewemor@gmail.com>
2023-04-12 09:27:45 +0200

    first commit

---
+++ adat.js
@@ -0,0 +1,7 @@
+export const ADATLISTA =[
+    {nev:"Dézi", fajta:"keverék", kor:12},
+    {nev:"Bodri", fajta:"mudi", kor:4},
+    {nev:"Loki", fajta:"keverék", kor:3},
+    {nev:"Zuri", fajta:"foxterrier", kor:8},
+    {nev:"Lappy", fajta:"Német Juhász", kor:4}
+]
---
+++ main.js
@@ -0,0 +1,84 @@
+import { ADATLISTA } from "./adat.js";
+import { rendezes } from "./rendezes.js";
+import { szures } from "./szures.js";
+$(function () {
+
+    rendezes(ADATLISTA, "kor");
+    rendezes(ADATLISTA, "nev");
+    rendezes(ADATLISTA, "fajta");
+
+    console.log(ADATLISTA);
+    console.log(szures(ADATLISTA, "fajta", "keverék"));
+
+    const TABLAZATELEM = $("#admin");
+    const NEVINPUTELEM = $("#nev");
+    const KORINPUTELEM = $("#kor");
+    const FAJTAINPUTELEM = $("#fajta");
+    const ADAFELVITEL = $("#kuldes");
+
+    let tartalom = megjelenit(ADATLISTA);
+    TABLAZATELEM.append(tartalom);
+
+
+    $("th").on("click", function () {
+        let index = $(this).index();
+        let kulcs = Object.keys(ADATLISTA[0])[index];
+
+        rendezes(ADATLISTA, kulcs);
+
+        let tablaElem = $("table");
+        let ujTartalom = megjelenit(ADATLISTA);
+        tablaElem.replaceWith(ujTartalom);
+    });
+
+   
+
+
+   
+
+    FAJTAINPUTELEM.on("input", function () {
+        let fajtaErtek = FAJTAINPUTELEM.val();
+        let szurtlista = szures(ADATLISTA, "fajta", fajtaErtek);
+        console.log(szurtlista);
+        let tartalom = megjelenit(szurtlista);
+        $("table").replaceWith(tartalom);
+    });
+
+
+})
+
+function megjelenit(lista) {
+    let txt = "";
+    txt += "<table class='table table-bordered'>";
+    txt += "<tr>";
+    for (let kulcs in lista[0]) {
+        txt += "<th class='table-dark'>" + kulcs + "</th>";
+    }
+    txt += "<th class='table-dark'></th>";
+    txt += "</tr>";
+    for (let index = 0; index < lista.length; index++) {
+        txt += "<tr>";
+        for (let kulcs in lista[index]) {
+            txt += "<td>" + lista[index][kulcs] + "</td>";
+        }
+        txt += "<td class='torol' id='" + index + "'>X</td>";
+        txt += "</tr>";
+    }
+    txt += "</table>";
+    return txt;
+}
+NEVINPUTELEM.on("input", function () {
+    let nevErtek = NEVINPUTELEM.val();
+    let szurtlista = szures(ADATLISTA, "nev", nevErtek);
+    console.log(szurtlista);
+    let tartalom = megjelenit(szurtlista);
+    $("table").replaceWith(tartalom);
+});
+
+KORINPUTELEM.on("input", function () {
+    let korErtek = KORINPUTELEM.val();
+    let szurtlista = szures(ADATLISTA, "kor", korErtek);
+    console.log(szurtlista);
+    let tartalom = megjelenit(szurtlista);
+    $("table").replaceWith(tartalom);
+});
